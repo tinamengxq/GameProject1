5b39eb956c27730583ab8d7d1c80896f9a50b889
tinamengxq <tinam5@uci.edu>
2026-02-18 13:00:50 -0800

    1

---
+++ Game Project 1/Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]private float playerSpeed = 2f;
+    [SerializeField]private Sprite playerSprite;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
